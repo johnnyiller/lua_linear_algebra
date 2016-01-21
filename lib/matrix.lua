ac8a690ba88a311251da18eea1679dd4bed462e0
Jeff Durand <jeff.durand@gmail.com>
2016-01-21 19:28:01 +0000

    matrix can calculate reduced row echelon form...

--- lib/matrix.lua
+++ lib/matrix.lua
@@ -3,24 +3,134 @@ local Matrix = {}
 -- lua is kind of like javascript but better and faster
 -- notice we are using a table to represent an array.  to 
 -- be honest I like this a bit better than using an array.
-function Matrix.new(rows,columns)
+-- we pass in references to zero and one so that we can use rational
+-- numbers if we want for our matrix
+function Matrix.new(rows,columns,zero,one)
+  local zero = zero or 0
+  local one = one or 1
+  
   local self = {}
-  -- sparse data structure for a matrix, really like this implemenation
-  function self.set(x,y,value)
-    if x <= rows and y <= columns then
-      if value == 0 then
-	self[x*columns + y] = nil	 
-      else
-	self[x*columns + y] = value
+  -- call a function for each x y pair
+  local function each(func)
+    for x=1, rows do
+      for y=1, columns do
+        func(x,y)
       end
     end
   end
+
+  -- sparse data structure for a matrix, really like this implemenation
+  function self.set(x,y,value)
+    if value == zero or x > rows or y > columns  then
+      self[x*columns + y] = nil 
+    else
+      self[x*columns + y] = value
+    end
+  end
+
   function self.get(x,y)
     local val = self[x*columns + y] 
     if val == nil then
-      return 0
+      return zero
     else
       return val
+    end
+  end
+
+  local function lead_zeros_for_row(row_index)
+    for i=1, columns do 
+      local value = self.get(row_index, i)
+      if value and value ~= zero then
+        return i - 1
+      end
+    end
+  end
+
+  local function swap_row(row_one, row_two)
+    -- double assignment allows quick swapping...
+    for i=1, columns do 
+      self[row_one*columns + i], self[row_two*columns + i] = self[row_two*columns + i], self[row_one*columns + i]
+    end
+
+  end
+
+  local function replace_row(pivot_row, row_two)
+
+            
+
+  end
+
+  function self.row_sort()
+    local item_count = rows
+    local has_changed
+    repeat 
+      has_changed = false
+      item_count = item_count - 1
+      for i = 1, item_count do 
+        if lead_zeros_for_row(i) > lead_zeros_for_row(i+1) then
+          swap_row(i, i+1) 
+          has_changed = true
+        end
+      end
+    until has_changed == false
+  end
+
+  local function substitute_row(start_row, sub_row, pivot_column)
+
+    local pivot_value = self.get(start_row, pivot_column)
+    local pivot_basis = self.get(sub_row, pivot_column)
+
+    -- if we have actual numbers to operate on then we can do what
+    -- we do with them.
+    if pivot_value ~= zero and pivot_basis ~= zero then 
+
+      local factor = pivot_basis / pivot_value
+      local first_sub_value = factor * pivot_value
+
+      if pivot_basis - first_sub_value == zero then
+        for i=pivot_column, columns do 
+          local sub_amount = factor * self.get(start_row, i)
+          self.set(sub_row, i, self.get(sub_row, i) - sub_amount)
+        end
+      else
+        for i=pivot_column, columns do 
+          local sub_amount = factor * self.get(start_row, i)
+          self.set(sub_row, i, self.get(sub_row, i) + sub_amount)
+        end
+      end
+
+    end
+
+  end
+
+  function self.ref()
+    -- we first sort the matrix because that gives us a nice known starting
+    -- structure then we can take advantage of this structure to perform our
+    -- operations...
+    self.row_sort()
+    for row = 1, rows do 
+      local pivot_column = lead_zeros_for_row(row) + 1
+      for next_row = row + 1, rows do  
+        substitute_row(row, next_row, pivot_column)
+      end
+    end
+  end
+
+  function self.rref()
+    self.ref()    
+    for row=rows, 1, -1 do
+
+      local pivot_column = lead_zeros_for_row(row) + 1
+      local pivot_value = self.get(row, pivot_column)
+      -- scale row so that the pivot is a 1
+      for i=pivot_column, columns do
+        self.set(row, i, self.get(row,i) / pivot_value)  
+      end
+
+      for prev_row = row - 1, 1, -1 do 
+        substitute_row(row, prev_row, pivot_column)
+      end
+
     end
   end
 
@@ -32,22 +142,22 @@ function Matrix.new(rows,columns)
     -- start off the grid so that the first one is automatically valid
     local indexes = {}
     local left_index = 0
+	  -- get the value from the sparse matrix
     for x=1, rows do
-      for y=1, columns do 
-	-- get the value from the sparse matrix
-	local value = self.get(x,y)
-	if value and value > 0 then
-	  indexes[x] = y
-	  if y <= left_index then
-	    -- this is the primary condition for echelon form
-	    return false, indexes
-	  else
-	    left_index = y
-	    break
-	  end
-	end
+      for y=1, columns do
+        local value = self.get(x,y)
+        if value and value > zero then
+          indexes[x] = y
+          if y <= left_index then
+            -- this is the primary condition for echelon form
+            return false, indexes
+          else
+            left_index = y
+            break
+          end
+        end
       end
-    end 
+    end
     return true, indexes
   end
   self.is_ref = self.is_row_echelon_form
@@ -57,19 +167,20 @@ function Matrix.new(rows,columns)
     if not isref then
       return false
     end
+
     for index, column_index in ipairs(indexes)  do
       local val = self.get(index,column_index)  
-      if val == 1 then
-	local previous_row = index - 1
-	if previous_row >= 1 then 
-	  for x=previous_row,1 do
-	    if self.get(x, column_index) > 0 then
-	      return false, indexes
-	    end
-	  end
-	end
+      if val == one then
+        local previous_row = index - 1
+        if previous_row >= 1 then 
+          for x=previous_row,1 do
+            if self.get(x, column_index) > zero then
+              return false, indexes
+            end
+          end
+        end
       else
-	return false, indexes
+	      return false, indexes
       end
     end
     return true, indexes
@@ -77,17 +188,16 @@ function Matrix.new(rows,columns)
   self.is_rref = self.is_reduced_row_echelon_form
 
   function self.load(matrix_rows)
-    for i=1, rows do
-      for j=1, columns do
-	local val = matrix_rows[i][j]
-	if val == 0 then
-	  val = nil
-	end
-	self.set(i,j,val)
+    each(function(x,y)
+      local val = matrix_rows[x][y]
+      if val == zero then
+        val = nil
       end
-    end
+      self.set(x,y,val)
+    end)
   end
 
+  
   -- dump and load should make a copy of the matrix making
   -- we don't copy by default cause what if we have a giant
   -- matrix, instead we want to mutate the existing table most 
@@ -97,32 +207,30 @@ function Matrix.new(rows,columns)
     for x=1, rows do
       T[x] = {}
       for y=1, columns do
-	T[x][y] = self.get(x,y)
+	      T[x][y] = self.get(x,y)
       end
     end
     return T
   end
 
   -- print out a reasonably formatted matrix
-  local tostring = function()
+  local _matrix_tostring = function()
     local str = "\n"
-    for x=1, rows do
-      for y=1, columns do 
-	local val = self.get(x,y)
-	if val == nil then
-	  val = 0
-	end
-	str = str .. val .. " "
+    each(function(x,y)
+      local val = self.get(x,y)
+
+      str = str .. tostring(val) .. " "
+      if y == columns then
+        str = str .. "\n"
       end
-      str = str .. "\n"
-    end
+    end)
     return str
   end
-  
+   
   -- meta table is like built in methods for the
   -- object, they are used by other built ins from 
   -- what I can tell
-  setmetatable(self, { __tostring = tostring })
+  setmetatable(self, { __tostring = _matrix_tostring })
 
   return self
 end
--- spec.lua
+++ spec.lua
@@ -1,10 +1,10 @@
 require 'busted.runner'()
+local Matrix = require("lib.matrix")
 
 describe("Matrix", function()
   randomize()
   describe("getters and setters", function()
     it("sets and gets withing the bound of the matrix", function()
-      local Matrix = require("lib.matrix")
       local mtrx = Matrix.new(3,3) 
       local mtrxtwo = Matrix.new(3,3) 
       mtrx.set(1,1,78)
@@ -20,7 +20,6 @@ describe("Matrix", function()
 
   describe("utility functions", function()
     it("should dump the matrix to a list of lists", function()
-      local Matrix = require("lib.matrix")
       local matrix = Matrix.new(3,4)
       matrix.set(1,1,12)
       matrix.set(2,2,9)
@@ -30,11 +29,10 @@ describe("Matrix", function()
       assert.same(9, d[2][2])
     end)
     it("should load a matrix from a list of lists", function()
-      local Matrix = require("lib.matrix")
       local matrix = Matrix.new(2,3)
       matrix.load({
-	{1,2,3},
-	{7,8,9}
+	      {1,2,3},
+	      {7,8,9}
       })
       assert.same(8, matrix.get(2,2)) 
     end)
@@ -42,7 +40,6 @@ describe("Matrix", function()
 
   describe("form operations", function()
     it("should test for row echelon form", function()
-      local Matrix = require("lib.matrix")
       local matrix = Matrix.new(3,4)
       matrix.set(1,1,12)
       matrix.set(2,2,9)
@@ -54,50 +51,132 @@ describe("Matrix", function()
     end) 
 
     it("should test for reduced row echelon form", function()
-      local Matrix = require("lib.matrix")
       local matrix = Matrix.new(5,10)
 
       local matrix_rows = {
-	{0,6,1,1,1,1,1,-1,1,1},
-	{0,0,0,2,1,-1,1,1,1,1},
-	{0,0,0,0,3,-1,1,1,1,1},
-	{0,0,0,0,0,2,-1,1,1,1},
-	{0,0,0,0,0,0,0,0,-9,1}
+        {0,6,1,1,1,1,1,-1,1,1},
+        {0,0,0,2,1,-1,1,1,1,1},
+        {0,0,0,0,3,-1,1,1,1,1},
+        {0,0,0,0,0,2,-1,1,1,1},
+        {0,0,0,0,0,0,0,0,-9,1}
       }
       matrix.load(matrix_rows)
       assert.falsy(matrix.is_reduced_row_echelon_form())
       
       matrix_rows = {
-	{0,1,1,1,1,1,1,-1,1,1},
-	{0,0,0,1,1,-1,1,1,1,1},
-	{0,0,0,0,1,-1,1,1,1,1},
-	{0,0,0,0,0,1,-1,1,1,1},
-	{0,0,0,0,0,0,0,0,1,1}
+        {0,1,1,1,1,1,1,-1,1,1},
+        {0,0,0,1,1,-1,1,1,1,1},
+        {0,0,0,0,1,-1,1,1,1,1},
+        {0,0,0,0,0,1,-1,1,1,1},
+        {0,0,0,0,0,0,0,0,1,1}
       }
       matrix.load(matrix_rows)
       assert.falsy(matrix.is_reduced_row_echelon_form())
       
       matrix_rows = {
-	{0,1,6,0,0,0,3,3,0,7},
-	{0,0,0,1,0,0,3,3,0,5},
-	{0,0,0,0,1,0,3,3,0,4},
-	{0,0,0,0,0,1,1,1,0,3},
-	{0,0,0,0,0,0,0,0,1,1}
+        {0,1,6,0,0,0,3,3,0,7},
+        {0,0,0,1,0,0,3,3,0,5},
+        {0,0,0,0,1,0,3,3,0,4},
+        {0,0,0,0,0,1,1,1,0,3},
+        {0,0,0,0,0,0,0,0,1,1}
       }
       matrix.load(matrix_rows)
       assert.truthy(matrix.is_reduced_row_echelon_form())
 
       local matrix = Matrix.new(4,4)
       matrix_rows = {
-	{1,0,3,6},
-	{0,1,3,1},
-	{0,0,0,0},
-	{0,0,0,0}
+        {1,0,3,6},
+        {0,1,3,1},
+        {0,0,0,0},
+        {0,0,0,0}
       }
       matrix.load(matrix_rows)
       assert.truthy(matrix.is_reduced_row_echelon_form())
 
+      local r = require('rational') 
+      local matrix = Matrix.new(4,4, r(0/1), r(1/1))
+      matrix_rows = {
+        {r(1,1),r(0,1),r(3,4),r(6,4)},
+        {r(0,1),r(1,1),r(3,1),r(1,1)},
+        {r(0,1),r(0,1),r(0,1),r(0,1)},
+        {r(0,1),r(0,1),r(0,1),r(0,1)}
+      }
+      matrix.load(matrix_rows)
+      assert.truthy(matrix.is_row_echelon_form())
+      assert.truthy(matrix.is_reduced_row_echelon_form())
+
+
     end)
   end)
+  describe("sort rows", function()
+
+    it("should be able to swap a row with another row mutating", function()
+      local r = require('rational')
+      local matrix = Matrix.new(4, 4, r(0,1), r(1,1))
+      matrix_rows = {
+        {r(0,1), r(3,2),r(2,1),r(2,5)},
+        {r(3,5), r(3,2),r(2,1),r(2,5)},
+        {r(0,1), r(0,1),r(2,1),r(2,5)},
+        {r(2,9), r(3,2),r(2,1),r(2,5)}
+      }
+      matrix.load(matrix_rows)
+      matrix.row_sort()
+      assert.equal(r(3,5), matrix.get(1,1))
+      assert.equal(r(3,2), matrix.get(3,2))
+      assert.equal(r(2,1), matrix.get(4,3))
+    end)
+  end)
+  
+  describe("ref", function()
+    it('should convert a matrix into reduced echelon form', function()
+      local r = require('rational')
+      local matrix = Matrix.new(3, 6, r(0,1), r(1,1))
+      matrix_rows = {
+        {r(0,1), r(3,1),r(-6,1),r(6,1),r(4,1),r(-5,1)},
+        {r(3,1), r(-7,1),r(8,1),r(-5,1),r(8,1),r(9,1)},
+        {r(3,1), r(-9,1),r(12,1),r(-9,1),r(6,1),r(15,1)}
+      }
+      matrix.load(matrix_rows)
+      matrix.ref()
+      assert.truthy(matrix.is_row_echelon_form())
+      
+    end)
+    it('should produce a specific form or ref for a properly sorted matrix', function()
+      local r = require('rational')
+      local matrix = Matrix.new(3, 6, r(0,1), r(1,1))
+      matrix_rows = {
+        {r(3,1), r(-9,1),r(12,1),r(-9,1),r(6,1),r(15,1)},
+        {r(3,1), r(-7,1),r(8,1),r(-5,1),r(8,1),r(9,1)},
+        {r(0,1), r(3,1),r(-6,1),r(6,1),r(4,1),r(-5,1)}
+      }
+      matrix.load(matrix_rows)
+      matrix.ref()
+      assert.equal(r(3,1), matrix.get(1,1))
+      assert.equal(r(2,1), matrix.get(2,2))
+      assert.equal(r(1,1), matrix.get(3,5))
+      assert.equal(r(0,1), matrix.get(3,1))
+      assert.equal(r(0,1), matrix.get(3,2))
+      assert.equal(r(0,1), matrix.get(3,3))
+
+    end)
+  end)
+
+  describe("rref", function()
+
+    it('should produce a specific form or ref for a properly sorted matrix', function()
+      local r = require('rational')
+      local matrix = Matrix.new(3, 6, r(0,1), r(1,1))
+      matrix_rows = {
+        {r(3,1), r(-9,1),r(12,1),r(-9,1),r(6,1),r(15,1)},
+        {r(3,1), r(-7,1),r(8,1),r(-5,1),r(8,1),r(9,1)},
+        {r(0,1), r(3,1),r(-6,1),r(6,1),r(4,1),r(-5,1)}
+      }
+      matrix.load(matrix_rows)
+      matrix.rref()
+      assert.truthy(matrix.is_reduced_row_echelon_form())
+    end)
+
+  end)
+
 
 end)
